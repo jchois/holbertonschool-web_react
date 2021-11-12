a35ffa16a4281f4c4e73221a0c40f38bd42aaab3
Juliana Chois <39529300+jchois@users.noreply.github.com>
2021-11-12 10:31:23 -0500

    deploy app

--- 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function NotificationItem(prop) {
-  if (prop.value) return (<li data-notification-type={prop.type}>{prop.value}</li>);
-  else return (<li data-notification-type={prop.type} dangerouslySetInnerHTML={prop.html}></li>);
-}
-
-export default NotificationItem;
